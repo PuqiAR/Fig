4dca43530786623ea83659514a40a724a2650f1d
PuqiAR <PuqiAR@qq.com>
2026-01-02 16:48:10 +0800

    test build again! again!

--- xmake.lua
+++ xmake.lua
@@ -5,10 +5,13 @@ set_policy("run.autobuild", false)
 
 target("Fig")
     set_kind("binary")
-    set_languages("c++23") 
-    
+    set_languages("c++23")
+
     if is_plat("windows") then
         set_plat("mingw")
+    end
+
+    if is_plat("mingw") then
         add_cxxflags("-static")
         add_cxxflags("-stdlib=libc++")
         add_ldflags("-Wl,--stack,268435456")
@@ -20,7 +23,7 @@ target("Fig")
     add_files("src/Lexer/lexer.cpp")
     add_files("src/Parser/parser.cpp")
     add_files("src/Value/value.cpp")
-    
+
     add_includedirs("src")
 
     set_warnings("all")
