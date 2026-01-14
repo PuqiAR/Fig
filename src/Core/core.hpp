310d79acc51fd9e0c258ef1ac32eb9330ea1eb0a
PuqiAR <PuqiAR@qq.com>
2026-01-14 17:34:24 +0800

    我忘记改版本号了

--- src/Core/core.hpp
+++ src/Core/core.hpp
@@ -4,7 +4,7 @@
 #include <cstdint>
 #include <string_view>
 
-#define __FCORE_VERSION "0.3.6-alpha"
+#define __FCORE_VERSION "0.3.7-alpha"
 
 #if defined(_WIN32)
     #define __FCORE_PLATFORM "Windows"
